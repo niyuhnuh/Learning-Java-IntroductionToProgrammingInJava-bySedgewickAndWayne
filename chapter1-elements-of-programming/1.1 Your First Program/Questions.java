b216b80f27111ff4aeb6462c48a2a6f16120b206
niyuhnuh <82695704+niyuhnuh@users.noreply.github.com>
2026-03-18 21:04:15 +0800

    add answers to chapter 1.1 exercises

---
+++ chapter1-elements-of-programming/1.1 Your First Program/Questions.java
@@ -0,0 +1,39 @@
+public class Questions {
+    public static void main(String[] args){
+        //1.1.1. Write a program that prints the "Hello, World" message 10 times.
+        //Solution #1:
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        //Solution #2:
+        for(int i=0; i <=9; i++){
+            System.out.println("Hello World!!!");
+        }
+
+        //1.1.2. Describe what happens if you omit the following in HelloWorld.java
+            //a. public = When removed, this shows a run-time error that the main method cannot be found, and shows the correct usage:  public static void main(String[] args)
+            //b. static = When removed, this shows another run-time error that main method is not static, and again shows the correct usage: public static void main(String[] args)
+            //c. void = When removed, this shows a compile-time error, saying that the return type is required and an approximate location of where the offending part is
+            //d. args = When removed, this shows a compile-time error, saying that an identifier is expected and an approximate location of where the offending part is 
+
+        //1.1.3. Describe what happens if you misspell the following in HelloWorld.java
+            //a. public = When misspelled, this shows a compile-time error and an approximate location of the offending word
+            //b. static = When misspelled, this shows a compile-time error and an approximate location of the offending word; and additionally another error for not having a valid return type
+            //c. void = When misspelled, this shows a compile-time error with offending word and the specific class where the offending word is located
+            //d. args = When misspelled, nothing really happened as this is a variable name
+        
+        //1.1.4. Describe what happens if you put the double quotes in the print statement of HelloWorld.java on different lines, as in this code fragment:
+            //When the print statements in between the quote marks are placed in different lines, a compile-time error shows exactly 4 errors (unclosed string literal, the expected ';' and not a statement)
+
+        //1.1.5. Describe what happens if you try to execute UseArgument with each of the following command lines
+            
+    }
+}
