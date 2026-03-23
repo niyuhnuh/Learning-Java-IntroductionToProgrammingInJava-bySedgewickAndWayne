553dc5801ae8dff01f54f77ae6985faf39a4422c
niyuhnuh <nianiaandrea@gmail.com>
2026-03-23 22:15:28 +0800

    add answers for 1.2.7 - 1.2.10

--- chapter1-elements-of-programming/1.2 Built-in Data Types/Questions.java
+++ chapter1-elements-of-programming/1.2 Built-in Data Types/Questions.java
@@ -43,6 +43,8 @@ public class Questions {
         // 1.2.4 Suppose that a and b are int variables. Simplify the following
         // expression: (!(a && b) && (a || b)) || ((a && b) || !(a || b))
 
+        // Answer:
+
         // 1.2.5 The exclusive or operator ^ for boolean operands is defined to be true
         // if they are different, false if they are the same. Give a truth table for
         // this function.
@@ -77,7 +79,76 @@ public class Questions {
         // d.System.out.println("bc" + (2 + 3));
         System.out.println("bc" + (2 + 3));
 
+        // Answer: This function first resolves the inner integer operation between the
+        // literals 2 and 3 however, it firstly prints the string literal before
+        // displaying the resulting value of the operation.
+
         // e.System.out.println("bc" + 2 + 3);
         System.out.println("bc" + 2 + 3);
+
+        // Answer: This function recognizes the first literal as a string and then
+        // therefore converts the succeeding literals as strings, and without the inner
+        // parentheses indicative of their grouping, the function call just displays the
+        // concatenation of the the three ltierals as strings.
+
+        // 1.2.8 Explain how to use PROGRAM 1.2.3 to find the square root of a number.
+
+        // Answer: This question refers to using the Quadratic.java program to find the
+        // square root of the number. The suggested refactor goes as to removing the
+        // unnecessary operations that were done in the previous lecture, retrieve an
+        // input from the user, and use the Math.sqrt() function call on that input.
+        // Lastly, call a print statement to display the result.
+
+        int arg = Integer.parseInt(args[0]);
+
+        double sqrt = Math.sqrt(arg);
+
+        System.err.println("This is the square root of the first input argument: " + sqrt);
+
+        // 1.2.9 What does each of the following print?
+
+        // a.System.out.println('b');
+        System.out.println('b');
+
+        // Answer: The aforementioned print statement displays the character b into the
+        // console.
+
+        // b.System.out.println('b' + 'c');
+        System.out.println('b' + 'c');
+
+        // Answer: This print statement takes the internal Unicode values of the
+        // characters and concatenates them together through the "+" operation.
+
+        // c.System.out.println((char) ('a' + 4));
+        System.out.println((char) ('a' + 4));
+
+        // Answer: This print statement casts a type conversion to the internal
+        // grouping, taking the char value of the summation of the integer literal 4's
+        // char value and the Unicode value of char a
+
+        // 1.2.10 Suppose that a variable a is declared as int a = 2147483647 (or
+        // equivalently, Integer.MAX_VALUE). What does each of the following print?
+
+        int aa = 2147483647;
+
+        // a.System.out.println(a);
+        System.out.println(aa);
+        // Answer:
+
+        // b.System.out.println(a + 1);
+        System.out.println(aa + 1);
+
+        // c.System.out.println(2 - a);
+        System.out.println(2 - aa);
+
+        // d.System.out.println(-2 - a);
+        System.out.println(-2 - aa);
+
+        // e.System.out.println(2 * a);
+        System.out.println(2 * aa);
+
+        // f.System.out.println(4 * a);
+        System.out.println(4 * aa);
+
     }
 }
